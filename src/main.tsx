68ba5daa30846fa973fa4c426894a5229f019f6c
tigawanna <denniskinuthiawaweru@gmail.com>
2024-10-23 19:13:45 +0300

    add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import "./reset.css";
 import "./vscode.css";
-import  "./my-css.css";
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
     <App />
