cda813859abc794949b6776ad90996ecf600204e
tigawanna <denniskinuthiawaweru@gmail.com>
2024-10-28 10:42:23 +0300

    slection working

--- src/App.tsx
+++ src/App.tsx
@@ -21,15 +21,15 @@ function App() {
             console.info({ inferredLabels });
             if (inferredLabels) {
               updateFormData({
-                content: message.data.selectedText || "",
+                content: inferredLabels.content,
                 title: inferredLabels.title,
                 description: inferredLabels.description,
               });
             }
 
-            updateFormData({
-              content: message.data.selectedText || "",
-            });
+            // updateFormData({
+            //   content: message.data.selectedText || "",
+            // });
           }
           break;
       }
@@ -59,42 +59,28 @@ function extractTitleAndDescription(content: string) {
   // ---Description: nicer code block---
   const lines = content.split("\n");
   const inferredLabels = lines.reduce(
-    (acc, line: string) => {
+    (acc, line: string,) => {
       if (line.includes("---Title")) {
         const [k, v] = line.split("---Title");
         acc["title"] = v.trim().replace("---", "").replace(":", "");
       }
-
       if (line.includes("---Description")) {
         const [k, v] = line.split("---Description");
         acc["description"] = v.trim().replace("---", "").replace(":", "");
+
       }
 
       return acc;
     },
-    { title: "Untitled", description: "" }
+    { title: "Untitled", description: ""}
   );
-  console.log({ inferredLabels });
-  // for (let i = 0; i < lines?.length; i++) {
-  //   const line = lines[i];
-  //   // if (title?.length > 0 && description?.length > 0) return;
-  //   if (line.includes("---")) {
-  //     console.log("============found======", line);
-  //     const [key, value] = line.split(":");
-  //     console.log({ key, value });
-  //     if (key.trim() === "Title") {
-  //       title = value.trim();
-  //     } else if (key.trim() === "Description") {
-  //       description = value.trim();
-  //     }
-  //   }
-  // }
-  // console.log({ title, description });
-  // if (title?.length === 0) {
-  //   title = "Untitled";
-  // }
-  // if (description?.length === 0) {
-  //   description = "";
-  // }
-  return inferredLabels;
+  const linesWithoutTitleAndDescription = lines
+  .filter((item)=>{
+    if(item.includes("---Title") || item.includes("---Description")){
+      return false
+    }
+    return true
+  })
+    .join("\n");
+  return { ...inferredLabels, content: linesWithoutTitleAndDescription };
 }
