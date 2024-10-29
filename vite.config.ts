46f4eb3d5114b91ad6d5261eba24835c431907f4
tigawanna <denniskinuthiawaweru@gmail.com>
2024-10-29 10:40:07 +0300

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 import vscode from '@tomjs/vite-plugin-vscode';
 import react from '@vitejs/plugin-react-swc';
-
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     vscode({
       extension: {
-        sourcemap: 'inline',
+        sourcemap: "inline",
       },
     }),
+    tsconfigPaths(),
   ],
 });
